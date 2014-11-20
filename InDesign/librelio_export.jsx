1b3e7d18b71e659676acd265cea1f012a99e2215
Librelio Dev <dev@librelio.com>
2014-11-20 05:40:44 +0100

    Renamed covers script and added compression

--- InDesign/librelio_export.jsx
+++
@@ -1,1 +0,0 @@
-﻿#target "InDesign"var newFolder = Folder.selectDialog ('Choose output folder');if (newFolder != null)	app.documents[0].exportFile(ExportFormat.PDF_TYPE, File(newFolder.fullName.toString() + '\/' + newFolder.name.toString() + '.pdf'), false, app.pdfExportPresets.itemByName('[Smallest File Size]'));
